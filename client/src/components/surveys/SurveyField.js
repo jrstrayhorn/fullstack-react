030669b344051378ca004207492fdaeed484ee95
stepforwardtech <jrstrayhorn@gmail.com>
2018-01-12 15:58:40 -0500

    Working on Redux Form validation

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,16 @@
+// SurveyField contains logic to render a single
+// label and text input
+
+import React from 'react';
+
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+      {touched && error}
+    </div>
+  );
+};
+
+export default SurveyField;
